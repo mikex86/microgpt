cec30f4801c447a8d3021c163f7e6a213cc0d161
mikex86 <me.michael.kei@outlook.de>
2023-04-05 17:37:50 +0200

    host owt on aws

--- mains/openwebtext/training_memgpt_owt.py
+++ mains/openwebtext/training_memgpt_owt.py
@@ -1,3 +1,5 @@
+import os
+
 import torch
 import numpy as np
 
@@ -5,18 +7,47 @@ from models.memgpt import MemGptModel, MemGptConfig
 from train.logging import set_log_project_name
 from train.training import TrainingConfig, LanguageModelTrainer
 from data.dataset import BinaryTokenDataset
+from urllib.request import urlretrieve
+import progressbar
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
+def download_dataset():
+    class DownloadProgressBar:
+        def __init__(self):
+            self.pbar = None
+
+        def __call__(self, block_num, block_size, total_size):
+            if not self.pbar:
+                self.pbar = progressbar.ProgressBar(maxval=total_size)
+                self.pbar.start()
+
+            downloaded = block_num * block_size
+            if downloaded < total_size:
+                self.pbar.update(downloaded)
+            else:
+                self.pbar.finish()
+
+    if not os.path.exists("datasets/openwebtext_gpt2/train.bin"):
+        urlretrieve("https://micro-gpt-datasets.s3.eu-central-1.amazonaws.com/train.bin",
+                    "datasets/openwebtext_gpt2/train.bin", DownloadProgressBar())
+    if not os.path.exists("datasets/openwebtext_gpt2/val.bin"):
+        urlretrieve("https://micro-gpt-datasets.s3.eu-central-1.amazonaws.com/val.bin",
+                    "datasets/openwebtext_gpt2/val.bin", DownloadProgressBar())
+
+
 def main():
     set_log_project_name("memgpt-owt")
+
+    download_dataset()
+
     memgpt_config = MemGptConfig(
         block_size=256,
         n_windows=8,
         n_layers=6,
-        n_heads=6,
-        n_embd=384
+        n_heads=4,
+        n_embd=256
     )
 
     train_ds = BinaryTokenDataset(
@@ -37,8 +68,8 @@ def main():
         batch_size=4,
         n_mini_steps=4,
 
-        min_learning_rate=1e-4,
-        max_learning_rate=1e-3,
+        min_learning_rate=1e-6,
+        max_learning_rate=1e-4,
         warmup_steps=100,
         max_steps=5000,
 
@@ -50,8 +81,8 @@ def main():
         device=device,
         dtype=torch.float16,
 
-        evaluation_period=25,
-        num_evaluation_steps=32,
+        evaluation_period=50,
+        num_evaluation_steps=12,
 
         checkpoint_dir_path="checkpoints/owt/memgpt_checkpoints",
 
